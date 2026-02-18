fc8bc8df9d367eeab1b3228aae984507d7d686c4
MMAMAGOT <mmamagot7@gmail.com>
2026-02-18 22:24:01 +0900

    deploy fix
    :wq

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+<<<<<<< HEAD
 plugins: [react()],
 base: '/MyIntegrity_01/',
+=======
+  plugins: [react()],
+  base: "/my-integrity/",
+>>>>>>> 475db97 (deploy fix)
 })
